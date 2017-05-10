62547c3e4fb2fb2f324efcc51d2b492904964397
Steven Orvell <sorvell@google.com>
2017-05-10 10:12:19 -0700

    configure for firebase hosting.

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+var functions = require('firebase-functions');
+var fetch = require('node-fetch');
+
+exports.hn = functions.https.onRequest((request, response) => {
+  let path = request.path.slice(4);
+  // TODO(dfreedm): is there some static prop we can use?
+  let query = '';
+  for (let p in request.query) {
+    query = query || '?';
+    query += `${p}=${request.query[p]}`;
+  }
+  let url = `https://node-hnapi.herokuapp.com/${path}${query}`;
+  fetch(url).then(resp => {
+    resp.json().then(json => {
+      response.set('Cache-Control', 'public, max-age=300, s-maxage=300 stale-while-revalidate=6000');
+      response.send(json);
+    });
+  });
+});
